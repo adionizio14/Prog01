8c337ff45ca3ed741043d926ddcc2152d0fd997e
adionizio14 <andrew_dionizio@uri.edu>
2023-09-15 19:05:04 -0400

    added comments to code

--- Source/prog01v1.c
+++ Source/prog01v1.c
@@ -12,12 +12,13 @@ void print_divisors(int divisor_list[], int count);
 bool argument_check(char exe_name[], int num_args);
 
 int main(int argc, char* argv[]) {
-
+    //check that the number of args is correct, program ends if not
     if (argument_check(argv[0], argc))
     {
         return 0;
     }
     else {
+        //gets the number of divsors and prints out list
         int num;
         sscanf(argv[1], "%d", &num);
         int count = divisors_count(num);
@@ -28,7 +29,7 @@ int main(int argc, char* argv[]) {
 }
 
 int divisors_count(int num){
-
+    // Method to count the number of divsors the arguement has
     int count = 0;
     for (int i = 1; i <= num ; i++){
         if (is_divisor(i, num)){
@@ -39,7 +40,7 @@ int divisors_count(int num){
 }
 
 bool is_divisor(int check, int num){
-
+    // Method to check if a number is a divsor or not
     if(num % check == 0){
         return true;
     }
@@ -49,7 +50,7 @@ bool is_divisor(int check, int num){
 }
 
 void divisors(int count, int num){
-
+    // Method that gets the list of divisors
     int divisor_list[count];
     int list_position = 0;
     for (int i = 1; i <= num ; i++){
@@ -62,7 +63,7 @@ void divisors(int count, int num){
 }
 
 void print_divisors(int divisor_list[], int count){
-
+    //method that prints the list of divisiors
     printf("The list of divisors are ");
     for(int i = 0; i < count; i++){
         if (i == (count - 1)){
@@ -76,6 +77,7 @@ void print_divisors(int divisor_list[], int count){
 }
 
 bool argument_check(char exe_name[], int num_args){
+    //Method that checks the correct amount of arguments are given
     if (num_args == 1) {
         printf("program launched with no argument.\nProper usage: %s m \n", exe_name);
         return true;
